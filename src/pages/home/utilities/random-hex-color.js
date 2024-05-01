c4e6ea7d528e873d59488036709db08fa913af4e
Kyle Angelo Galendez <xcxkyleangelo26@gmail.com>
2024-05-01 18:00:31 +0800

    Implement randomHexColor utility
    Implemented a utility function named randomHexColor, which generates a
    random hexadecimal color code. The function generates random values for
    red, green, and blue components within the valid range (0-255) and
    converts them to hexadecimal format. Finally, it concatenates these
    values to form a valid hexadecimal color code and returns it.

---
+++ src/pages/home/utilities/random-hex-color.js
@@ -0,0 +1,15 @@
+'use strict'
+
+function randomHexColor() {
+	const red = Math.floor(Math.random() * 256)
+	const green = Math.floor(Math.random() * 256)
+	const blue = Math.floor(Math.random() * 256)
+
+	const hexRed = red.toString(16).padStart(2, '0')
+	const hexGreen = green.toString(16).padStart(2, '0')
+	const hexBlue = blue.toString(16).padStart(2, '0')
+
+	return '#' + hexRed + hexGreen + hexBlue
+}
+
+export { randomHexColor }
